776633f8e694dbb17c6ffd8a667f4b85a04754b9
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2016-08-23 22:55:20 +0200

    Employee model added

---
+++ library/src/main/java/com/at/library/model/Employee.java
@@ -0,0 +1,10 @@
+package com.at.library.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Employee extends User {
+
+	private static final long serialVersionUID = 3926211548637428563L;
+
+}

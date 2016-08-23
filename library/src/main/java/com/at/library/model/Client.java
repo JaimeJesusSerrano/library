7879c3607747db2276eb60fdf855aec4bd4d7c4e
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2016-08-23 22:49:54 +0200

    Client model added

---
+++ library/src/main/java/com/at/library/model/Client.java
@@ -0,0 +1,10 @@
+package com.at.library.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Client extends User {
+
+	private static final long serialVersionUID = -24600065830793754L;
+
+}

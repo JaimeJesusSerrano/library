d7a1c4c1ca593085ab404b8425302cbe7977ca9d
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2016-08-23 23:23:10 +0200

    Bookshelf model added

---
+++ library/src/main/java/com/at/library/model/Bookshelf.java
@@ -0,0 +1,35 @@
+package com.at.library.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Bookshelf implements Serializable {
+
+	private static final long serialVersionUID = -7067308147736914174L;
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}

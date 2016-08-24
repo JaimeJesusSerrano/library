5fbf775ff4ac54c257b222ca307f50816109cd4a
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2016-08-24 14:32:50 +0200

    Example connection JSON on test

--- library/src/test/java/com/at/LibraryApplicationTests.java
+++ library/src/test/java/com/at/LibraryApplicationTests.java
@@ -1,11 +1,16 @@
 package com.at;
 
 import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
+
+import com.at.library.dto.BookDTO;
 
 public class LibraryApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		final String url = "http//:localhost:8080/book";
+		final BookDTO bookDTO = new RestTemplate().getForObject(url, BookDTO.class);
 	}
 
 }

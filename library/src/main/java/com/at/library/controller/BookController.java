457689f14374a3e9f484a6c02bc0a16b9cb382ff
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2016-08-25 20:30:08 +0200

    Deleted comments

--- library/src/main/java/com/at/library/controller/BookController.java
+++ library/src/main/java/com/at/library/controller/BookController.java
@@ -28,33 +28,29 @@ public class BookController {
 		log.debug(String.format("Buscando todos los libros del sistema"));
 		return bookservice.findAll();
 	}
-	
-//	Crear
+
 	@RequestMapping(method = { RequestMethod.POST })
 	public BookDTO create(@RequestBody BookDTO bookDTO) {
 		log.debug(String.format("Vamos a crear el libro siguiente %s", bookDTO));
 		return bookservice.create(bookDTO);
 	}
-	
-//	Recuperar
+
 	@RequestMapping(value = "/{id}", method = { RequestMethod.GET })
 	public BookDTO findById(@PathVariable("id") Integer id) {
 		log.debug(String.format("Vamos a recuperar el libro con el siguiente id %s", id));
 		return bookservice.findById(id);
 	}
-	
-//	Modificar
+
 	@RequestMapping(value = "/{id}", method = { RequestMethod.PUT })
 	public void update(@PathVariable("id") Integer id, @RequestBody BookDTO bookDTO) {
 		log.debug(String.format("Vamos a actualizar el libro con el siguiente id %s", id));
 		bookservice.update(bookDTO);
 	}
-	
-//	Borrar
+
 	@RequestMapping(value = "/{id}", method = { RequestMethod.DELETE })
 	public void delete(@PathVariable("id") Integer id) {
 		log.debug(String.format("Vamos a actualizar el libro con el siguiente id %s", id));
-		bookservice.delete(id);;
+		bookservice.delete(id);
 	}
 
 }

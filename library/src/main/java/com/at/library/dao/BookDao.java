0f1f83e446047fc6319d36e301e56e3e03ef26bc
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2016-08-26 14:10:33 +0200

    Example Query added

--- library/src/main/java/com/at/library/dao/BookDao.java
+++ library/src/main/java/com/at/library/dao/BookDao.java
@@ -8,4 +8,6 @@ import com.at.library.model.Book;
 @Repository
 public interface BookDao extends CrudRepository<Book, Integer> {
 
+//	@Query (value="SELECT new com.at.library.dto.BookDTO(b.id, b.title, b.author) FROM Book AS b WHERE b.id IN (SELECT r.pk.book FROM Rent AS r WHERE r.endDate IS null)")
+//public List<BookDTO> findUnavailable();
 }

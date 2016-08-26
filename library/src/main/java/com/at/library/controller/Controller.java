e81d2e5457e7de82fd79414dcce47ed113414768
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2016-08-26 14:10:09 +0200

    Generic controller added

---
+++ library/src/main/java/com/at/library/controller/Controller.java
@@ -0,0 +1,27 @@
+package com.at.library.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public abstract class Controller<T> {
+
+	@RequestMapping(method = { RequestMethod.GET })
+	public abstract List<T> getAll();
+
+	@RequestMapping(method = { RequestMethod.POST })
+	public abstract T create(@RequestBody T t);
+
+	@RequestMapping(value = "/{id}", method = { RequestMethod.GET })
+	public abstract T findById(@PathVariable("id") Integer id);
+
+	@RequestMapping(value = "/{id}", method = { RequestMethod.PUT })
+	public abstract void update(@PathVariable("id") Integer id, @RequestBody T t);
+
+	@RequestMapping(value = "/{id}", method = { RequestMethod.DELETE })
+	public abstract void delete(@PathVariable("id") Integer id);
+	
+}

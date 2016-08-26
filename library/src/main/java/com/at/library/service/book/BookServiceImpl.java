13997e1b6c57b89929dc540e88093e5885a00ca5
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2016-08-26 21:49:20 +0200

    Book model may not have an association with Bookshelf

--- library/src/main/java/com/at/library/service/book/BookServiceImpl.java
+++ library/src/main/java/com/at/library/service/book/BookServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.at.library.controller.BookController;
 import com.at.library.dao.BookDao;
 import com.at.library.dto.BookDTO;
 import com.at.library.model.Book;

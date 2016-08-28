0b6af2bc7eb002aace4459767d2fd362f3e0a8b1
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2016-08-28 13:10:00 +0200

    Status enum (ACTIVE and DISABLE) added to Client

--- library/src/main/java/com/at/library/model/Client.java
+++ library/src/main/java/com/at/library/model/Client.java
@@ -3,15 +3,22 @@ package com.at.library.model;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
+
+import com.at.library.enums.StatusEnum;
 
 @Entity
 @PrimaryKeyJoinColumn(referencedColumnName = "id")
 public class Client extends User {
 
 	private static final long serialVersionUID = -24600065830793754L;
+	
+	@Enumerated(EnumType.STRING)
+	private StatusEnum status;
 
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "client")
 	private List<Rent> rents;
@@ -22,6 +29,14 @@ public class Client extends User {
 
 	public void setRents(List<Rent> rents) {
 		this.rents = rents;
+	}
+
+	public StatusEnum getStatus() {
+		return status;
+	}
+
+	public void setStatus(StatusEnum status) {
+		this.status = status;
 	}
 
 }

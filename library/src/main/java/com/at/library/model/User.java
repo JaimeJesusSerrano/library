243f71e6336cdc7b47f81fbc6753d652dd6e9032
JaimeJesusSerrano <jaimejesusserrano@gmail.com>
2016-09-02 13:55:57 +0200

    User model updated with status and UserStatusEnum added

--- library/src/main/java/com/at/library/model/User.java
+++ library/src/main/java/com/at/library/model/User.java
@@ -3,9 +3,13 @@ package com.at.library.model;
 import java.util.List;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.PrimaryKeyJoinColumn;
+
+import com.at.library.enums.UserStatusEnum;
 
 @Entity
 @PrimaryKeyJoinColumn(referencedColumnName = "id")
@@ -15,13 +19,29 @@ public class User extends Person {
 
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "user")
 	private List<Rent> rents;
-
+	
+	@Enumerated(EnumType.STRING)
+	private UserStatusEnum status;
+	
 	public List<Rent> getRents() {
 		return rents;
 	}
 
 	public void setRents(List<Rent> rents) {
 		this.rents = rents;
+	}
+
+	public UserStatusEnum getStatus() {
+		return status;
+	}
+
+	public void setStatus(UserStatusEnum status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "User [rents=" + rents + ", status=" + status + "]";
 	}
 
 }
